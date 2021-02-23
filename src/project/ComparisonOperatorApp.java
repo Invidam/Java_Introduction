a49b62689c197608019a9b58fb130d10b51db4da
Invidam <hansupark0208@gmail.com>
2021-02-23 14:46:33 +0000

    0223

---
+++ src/project/ComparisonOperatorApp.java
@@ -0,0 +1,10 @@
+package project;
+
+public class ComparisonOperatorApp {
+    public static void main(String[] args)
+    {
+        System.out.println(1 > 1);
+        System.out.println(1 == 1);
+        System.out.println(1 != 1);
+    }
+}
